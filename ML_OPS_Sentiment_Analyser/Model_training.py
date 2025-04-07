225ca86ebcdd77437442ada8f55bad33013aac41
Manikandan M <53110404+Mani31899@users.noreply.github.com>
2025-04-07 01:48:02 -0400

    Update Model_training.py

--- ML_OPS_Sentiment_Analyser/Model_training.py
+++ ML_OPS_Sentiment_Analyser/Model_training.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+from model_wrapper import SentimentModel
 
 # ========== Logging ==========
 logging.basicConfig(
