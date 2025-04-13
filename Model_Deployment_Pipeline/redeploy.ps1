8de5db3d71309a08d4f6cbb034381dc10e70c7de
Manikandan M <53110404+Mani31899@users.noreply.github.com>
2025-04-13 14:21:32 -0400

    Update redeploy.ps1

--- Model_Deployment_Pipeline/redeploy.ps1
+++ Model_Deployment_Pipeline/redeploy.ps1
@@ -10,6 +10,10 @@ docker push gcr.io/mlops-project-test-448822/sentiment-analyzer-app
 Write-Host "Restarting Kubernetes deployment..."
 kubectl rollout restart deployment sentiment-analyzer
 
+
+Write-Host "Google Cloud Secret Key"
+kubectl create secret generic gcp-key-secret --from-file=gcp_key.json=./.secrets/gcp_key.json
+
 Write-Host "Re-applying Kubernetes YAML (if updated)..."
 kubectl apply -f k8s.yaml
 
