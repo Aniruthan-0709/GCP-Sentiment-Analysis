622d6165bd493fd4dab22ee4cb993d5ac08fd938
Manikandan M <53110404+Mani31899@users.noreply.github.com>
2025-04-19 19:07:11 -0400

    Update gcs_utils.py

--- Data_Pipeline/utils/gcs_utils.py
+++ Data_Pipeline/utils/gcs_utils.py
@@ -1,5 +1,7 @@
 import os
 from google.cloud import storage
+from io import BytesIO
+
 
 def download_from_gcp(bucket_name, source_blob_name, destination_file_name):
     if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"):
@@ -21,3 +23,16 @@ def upload_to_gcp(bucket_name, local_file_path, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(local_file_path)
     print(f"✅ Uploaded {local_file_path} to gs://{bucket_name}/{destination_blob_name}")
+
+def read_csv_from_gcs(bucket_name, blob_name):
+    from google.cloud import storage
+    import pandas as pd
+
+    if not os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"):
+        raise EnvironmentError("GOOGLE_APPLICATION_CREDENTIALS not set")
+
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    content = blob.download_as_bytes()
+    return pd.read_csv(BytesIO(content))
